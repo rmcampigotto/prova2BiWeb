3b961bad759ae2c8e250209151db2ea20d1c6704
Joao Henrique <salvalagiojoaoabrahim@gmail.com>
2024-06-11 22:10:21 -0300

    codeRefactor-116

--- main.js
+++ main.js
@@ -1,333 +1,226 @@
-document.addEventListener('DOMContentLoaded', () => {
-    getApiDefault()
-    botaoDialog()
-    history.replaceState(null, "", "http://127.0.0.1:3001/index.html?tipo=noticia&qtd=10")
-    controls.createListeners()
-})
+const search_input = document.getElementById("search-noticia")
+const total_filtro = document.getElementById("circulo-filtro")
+const filtro_tipo = document.getElementById("filtro-tipo")
+const filtro_quantidade = document.getElementById("filtro-quantidade")
+const filtro_de = document.getElementById("filtro-de")
+const filtro_ate = document.getElementById("filtro-ate")
 
-async function criaList(list, items) {
-    items.forEach(element => {
-        const divNoticia = document.createElement('div')
-        divNoticia.className = "divNoticia"
+const filtro = document.getElementById("svg-filtro");
+const dialog_filtro = document.getElementById("dialog-filtro");
+const close_dialog = document.getElementById("close-dialog");
 
-        const divFull = document.createElement('div')
-        divFull.className = "divFull"
+const paginacao = document.getElementById("paginacao");
 
-        const titulo = document.createElement('p')
-        titulo.className = "noticiaTitulo"
-        titulo.textContent = element.titulo
+filtro.addEventListener('click', () => {
+    dialog_filtro.showModal();
+});
 
-        const resumo = document.createElement('p')
-        resumo.className = "noticiaResumo"
-        resumo.textContent = element.introducao
+close_dialog.addEventListener('click', () => {
+    dialog_filtro.close();
+});
 
-        const editora = document.createElement('p')
-        editora.className = "noticiasEditora"
-        const editoras = (element.editorias).split(';')
-        editoras.forEach(edit => {
-            editora.textContent = editora.textContent + `#${edit} `
-        })
+const ul_noticia = document.getElementById("conteudo-principal");
 
-        const data = document.createElement('p')
-        data.className = "dataPubli"
 
-        let dia = element.data_publicacao.slice(0, 2)
-        let mes = element.data_publicacao.slice(3, 5)
-        let ano = element.data_publicacao.slice(6, 10)
-        let hora = element.data_publicacao.slice(11, 13)
-        let minuto = element.data_publicacao.slice(14, 16)
-        let segundo = element.data_publicacao.slice(17, 19)
+trazerInformacoesFiltradas();
 
-        let publichDate = new Date(ano, mes, dia, hora, minuto, segundo)
-        let actualDate = new Date(Date.now())
-
-        let dataMilisegundos = actualDate.getTime() - publichDate.getTime()
-        let diferenca = Math.ceil(dataMilisegundos / (1000 * 60 * 60 * 24))
-
-        diferenca = diferenca + 30
-
-        if (diferenca == 0) {
-            data.textContent = 'Publicado hoje'
-        } else if (diferenca == 1) {
-            data.textContent = 'Publicado Ontem'
-        } else {
-            data.textContent = `Publicado há ${diferenca} dias`
+function trazerInformacoesFiltradas() {
+    setFiltros()
+    getQuantidadeFiltros();
+    getNoticias().then((noticias) => {
+        while (ul_noticia.lastChild) {
+            ul_noticia.removeChild(ul_noticia.lastChild);
         }
-
-        const imagens = JSON.parse(element.imagens)
-        const divImagens = document.createElement('div')
-        divImagens.className = "divImagem"
-        const imagem = document.createElement('img')
-        imagem.src = "https://agenciadenoticias.ibge.gov.br/" + imagens.image_intro
-
-        const botao = document.createElement('button')
-        botao.className = "leiaMais"
-        const link = document.createElement('a')
-        link.href = element.link
-        link.textContent = "Leia mais"
-        botao.appendChild(link)
-
-        divImagens.appendChild(imagem)
-        divNoticia.appendChild(titulo)
-        divNoticia.appendChild(resumo)
-        divNoticia.appendChild(editora)
-        divNoticia.appendChild(data)
-        divNoticia.appendChild(botao)
-        divFull.appendChild(divImagens)
-        divFull.appendChild(divNoticia)
-        //list.appendChild(divFull)
-
-        listaPages.data.push(divFull)
-
-    })
-    state.totalPages = Math.ceil(items.length / perPage)
-    listaPages.update()
+        createCardsWithNoticias(noticias);
+        criarPaginas(noticias.totalPages, noticias.page);
+    });
 }
 
-function addParamUrl(paramName, paramValue) {
-
-    const urlParams = new URLSearchParams(window.location.search)
-
-    if (urlParams.has(paramName)) {
-        urlParams.delete(paramName)
-    }
-    urlParams.append(paramName, paramValue)
-
-    const novaURL = `${location.origin}${location.pathname}?${urlParams.toString()}`
-
-    history.replaceState(null, "", novaURL)
-
+async function getNoticias() {
+    const dados = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/${window.location.search}`);
+    return await dados.json();
 }
 
-async function callApiParams() {
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const urlReturn = new URLSearchParams()
-
-    const tipo = urlParams.get("tipo")
-    const qtd = urlParams.get("qtd")
-    const busca = urlParams.get("busca")
-    const de = urlParams.get("de")
-    const ate = urlParams.get("ate")
-
-    if (tipo != "" && tipo != null) {
-        urlReturn.append("tipo", tipo)
-    }
-    if (qtd != "" && qtd != null) {
-        urlReturn.append("qtd", qtd)
-    }
-    if (busca != "" && busca != null) {
-        urlReturn.append("busca", busca)
-    }
-    if (de != "" && de != null) {
-        urlReturn.append("de", de)
-    }
-    if (ate != "" && ate != null) {
-        urlReturn.append("ate", ate)
-    }
-
-    return `https://servicodados.ibge.gov.br/api/v3/noticias?${urlReturn.toString()}`
+function createCardsWithNoticias(noticias) {
+    noticias.items.forEach(n => adicionarFilho(ul_noticia, createCard(n)));
 }
 
-async function getApiDefault() {
-    const url = await callApiParams()
-    const result = await fetch(url)
-    const resultJson = await result.json()
-    const items = resultJson.items
-
-    const li = document.querySelector(".news")
-
-    await criaList(li, items)
-
+function setFiltros() {
+    const url = new URL(window.location);
+    search_input.value = url.searchParams.get('busca') ?? '';
+    filtro_tipo.value = url.searchParams.get('tipo') ?? '';
+    filtro_quantidade.value = url.searchParams.get('qtd') ?? '10';
+    filtro_de.value = url.searchParams.get('de') ?? '';
+    filtro_ate.value = url.searchParams.get('ate') ?? '';
+    url.searchParams.set('qtd', filtro_quantidade.value);
+    url.searchParams.set('page', url.searchParams.get('page') ?? '1');
+    window.history.pushState({}, '', url);
 }
 
-function botaoDialog() {
+function search(event) {
+    event.preventDefault();
+    const url = new URL(window.location);
+    url.searchParams.set('busca', search_input.value);
+    if (search_input.value === '') {
+        url.searchParams.delete('busca');
+    }
+    url.searchParams.set('page', '1');
+    window.history.pushState({}, '', url);
+    trazerInformacoesFiltradas();
+}
 
-    let updateButton = document.querySelector(".filtro");
-    let closeButton = document.querySelector(".fecharDialog")
-    let favDialog = document.querySelector("dialog");
+function aplicarFiltro(event) {
+    event.preventDefault();
+    const url = new URL(window.location);
+    url.searchParams.set('qtd', filtro_quantidade.value);
+    filtro_tipo.value ? url.searchParams.set('tipo', filtro_tipo.value) :
+        url.searchParams.delete('tipo');
+    filtro_de.value ? url.searchParams.set('de', filtro_de.value) :
+        url.searchParams.delete('de');
+    filtro_ate.value ? url.searchParams.set('ate', filtro_ate.value) :
+        url.searchParams.delete('ate');
+    dialog_filtro.close();
+    url.searchParams.set('page', '1');
+    window.history.pushState({}, '', url);
+    trazerInformacoesFiltradas();
+}
 
-    closeButton.addEventListener("onclick", function () {
-        favDialog.close()
-    })
+function getQuantidadeFiltros() {
+    const params = new URL(window.location).searchParams;
+    let totalFiltros = 0;
+    params.forEach((value, key) => {
+        if (key !== 'page' && key !== 'busca') {
+            totalFiltros++;
+        }
+    });
+    total_filtro.innerText = totalFiltros;
+}
 
-    updateButton.addEventListener("click", function () {
-        favDialog.showModal();
+function createCard(noticia) {
+    const li = criarElementoHTML('li');
+    const img = criarElementoHTML('img');
+    const divTexto = criarElementoHTML('div');
+    const titulo = criarElementoHTML('h2');
+    const paragrafo = criarElementoHTML('p');
+    const divSepararEditoriasPublicado = criarElementoHTML('div');
+    const editorias = criarElementoHTML('p');
+    const publicado = criarElementoHTML('p');
+    const botaoLerMais = criarElementoHTML('button');
+
+    img.src = getImagem(noticia.imagens);
+    img.alt = 'Imagem da notícia';
+
+    titulo.textContent = noticia.titulo;
+    paragrafo.textContent = noticia.introducao;
+    editorias.textContent = getEditorias(noticia.editorias);
+    publicado.textContent = getPublicado(noticia.data_publicacao);
+
+    divTexto.setAttribute('id', 'texto-listagem');
+
+    botaoLerMais.textContent = 'Ler Mais';
+    botaoLerMais.addEventListener('click', () => {
+        window.open(noticia.link, '_blank');
     });
 
+    adicionarClasses(botaoLerMais, ['width100', 'botao-noticia']);
+    adicionarClasses(divSepararEditoriasPublicado, ['flex', 'center-space-between']);
+    adicionarClasses(divTexto, ['width100', 'flex-column', 'gap-10']);
+    adicionarClasses(img, ['imagem-noticia']);
+    adicionarClasses(li, ['card-noticia']);
+
+    adicionarFilho(divSepararEditoriasPublicado, editorias);
+    adicionarFilho(divSepararEditoriasPublicado, publicado);
+
+    adicionarFilho(divTexto, titulo);
+    adicionarFilho(divTexto, paragrafo);
+    adicionarFilho(divTexto, divSepararEditoriasPublicado);
+    adicionarFilho(divTexto, botaoLerMais);
+
+    adicionarFilho(li, img);
+    adicionarFilho(li, divTexto);
+
+    return li;
 }
 
-async function searchFilter() {
-
-    const ul = document.querySelector(".listaNews")
-    const listOld = document.querySelector(".news")
-    ul.removeChild(listOld)
-
-    const listNew = document.createElement("li")
-    listNew.className = "news"
-    ul.appendChild(listNew)
-
-    const tipo = document.querySelector("#tipo")
-    const qtd = document.querySelector("#qtde")
-    const dataInicial = document.querySelector("#de")
-    const dataFinal = document.querySelector("#ate")
-    const input = document.querySelector(".search")
-
-    const tipoChoice = (tipo.options[tipo.selectedIndex].text).toLowerCase()
-    const qtdChoice = (qtd.options[qtd.selectedIndex].text)
-    const busca = input.value
-    const de = dataInicial.value
-    const ate = dataFinal.value
-
-    if (tipoChoice != "" && tipoChoice != null && tipoChoice != 'selecione') {
-        addParamUrl("tipo", tipoChoice)
-    }
-    if (qtdChoice != "") {
-        addParamUrl("qtd", qtdChoice)
-    }
-    if (busca != "") {
-        addParamUrl("busca", busca)
-    }
-    if (de != "") {
-        addParamUrl("de", de)
-    }
-    if (ate != "") {
-        addParamUrl("ate", ate)
-    }
-
-    const url = await callApiParams()
-
-    const result = await fetch(url)
-    const resultJson = await result.json()
-    const items = resultJson.items
-
-    await criaList(listNew, items)
-
+function getImagem(imagem) {
+    return 'https://agenciadenoticias.ibge.gov.br/'
+        + JSON.parse(!!imagem ? imagem : '{"image":{"image_intro": ""}}').image_intro;
 }
 
-async function buttonSearch() {
-
-    const ul = document.querySelector(".listaNews")
-    const listOld = document.querySelector(".news")
-
-    ul.removeChild(listOld)
-
-    const listNew = document.createElement("li")
-    listNew.className = "news"
-
-    ul.appendChild(listNew)
-
-    const tipo = document.querySelector("#tipo")
-    const qtd = document.querySelector("#qtde")
-    const dataInicial = document.querySelector("#de")
-    const dataFinal = document.querySelector("#ate")
-    const input = document.querySelector(".search")
-
-    const tipoChoice = (tipo.options[tipo.selectedIndex].text).toLowerCase()
-    const qtdChoice = (qtd.options[qtd.selectedIndex].text)
-    const busca = input.value
-    const de = dataInicial.value
-    const ate = dataFinal.value
-
-    if (tipoChoice != "" && tipoChoice != null && tipoChoice != 'selecione') {
-        addParamUrl("tipo", tipoChoice)
-    }
-    if (qtdChoice != "") {
-        addParamUrl("qtd", qtdChoice)
-    }
-    if (busca != "") {
-        addParamUrl("busca", busca)
-    }
-    if (de != "") {
-        addParamUrl("de", de)
-    }
-    if (ate != "") {
-        addParamUrl("ate", ate)
-    }
-
-    const url = await callApiParams()
-
-    const result = await fetch(url)
-    const resultJson = await result.json()
-    const items = resultJson.items
-
-    await criaList(listNew, items)
-
+function getEditorias(editorias) {
+    return '#' + editorias.replace(';', ' #');
 }
 
-//PAGINAÇÃO
-let perPage = 10
-const state = {
-    page: 1,
-    perPage,
-    totalPages: 0
+function getPublicado(dateString) {
+    const date = getFormatDate(dateString);
+    const dateHoje = new Date();
+    diferencaDatasEmDia = Math.round((dateHoje - date) / 24 / 60 / 60 / 1000);
+    if (diferencaDatasEmDia === 0) {
+        return 'Publicado hoje';
+    }
+    if (diferencaDatasEmDia === 1) {
+        return 'Publicado ontem';
+    }
+    return `Publicado há ${diferencaDatasEmDia} dias`;
 }
 
-const controls = {
-    next() {
-        state.page++
+function getFormatDate(dateString) {
+    const dateStringArray = dateString.split(' ');
+    const onlyDate = dateStringArray[0].split('/');
+    const day = onlyDate[0];
+    const month = onlyDate[1];
+    const year = onlyDate[2];
 
-        if (state.page > state.totalPages) {
-            state.page--
-        }
-        listaPages.update()
-    },
-    prev() {
-        state.page--
-
-        if (state.page < 1) {
-            state.page++
-        }
-        listaPages.update()
-    },
-    goTo(page) {
-
-        if (page < 1) {
-            page = 1
-        }
-
-        state.page = page
-
-        if (page > state.totalPages) {
-            state.page = state.totalPages
-        }
-        listaPages.update()
-    },
-    createListeners() {
-        document.querySelector('.first').addEventListener('click', () => {
-            this.goTo(1)
-        })
-        document.querySelector('.last').addEventListener('click', () => {
-            this.goTo(state.totalPages)
-        })
-        document.querySelector('.next').addEventListener('click', () => {
-            this.next()
-        })
-        document.querySelector('.prev').addEventListener('click', () => {
-            this.prev()
-        })
-    }
+    return new Date(`${year}-${month}-${day}T${dateStringArray[1]}Z`);
 }
 
-const listaPages = {
-    data: new Array(),
-    create(item) {
-        const list = document.querySelector(".news")
+function criarElementoHTML(element) {
+    return document.createElement(element);
+}
 
-        list.appendChild(item)
-    },
-    update() {
-        const list = document.querySelector(".news")
-        list.innerHTML = ""
+function adicionarClasses(element, classes) {
+    classes.forEach(c => {
+        element.classList.add(c);
+    })
+}
 
-        let page = state.page - 1
-        let start = page * state.perPage
-        let end = start + state.perPage
+function adicionarFilho(pai, filho) {
+    pai.appendChild(filho);
+}
 
-        const paginatedItems = this.data.slice(start, end)
-
-        paginatedItems.forEach( element => {
-            listaPages.create(element)
-        })
+function criarPaginas(totalPage, paginaAtual) {
+    let paginas = '';
+    let i = 1;
+    if (paginaAtual >= 7 && totalPage > 10) {
+        i = paginaAtual - 5
     }
+    if (paginaAtual >= totalPage - 4 && totalPage > 10) {
+        i = totalPage - 9;
+    }
+    const fimPagina = i + 9
+    while (i <= fimPagina && i !== totalPage + 1) {
+        paginas += criarPagina(i);
+        i++;
+    }
+    paginacao.innerHTML = paginas;
+}
+
+function criarPagina(index) {
+    const url = new URL(window.location);
+    const isAtiva = url.searchParams.get('page') === index.toString()
+    return `
+        <li>
+            <button 
+                class="${isAtiva ? 'pagina-ativa' : 'pagina'} width100 pointer" 
+                type="button" 
+                onclick="changePage(this)">${index}</button>
+        </li>
+    `
+}
+
+function changePage(element) {
+    const url = new URL(window.location);
+    url.searchParams.set('page', element.textContent);
+    window.history.pushState({}, '', url);
+    trazerInformacoesFiltradas()
 }
